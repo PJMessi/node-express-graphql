e61981cf7a958003659d24afcd2aa29d9a3cd40a
Prajwal Shrestha <prajwal.shrestha@hazesoft.co>
2020-11-04 23:11:40 +0545

    listed events in events page

--- vue/src/views/Event.vue
+++ vue/src/views/Event.vue
@@ -12,6 +12,34 @@
       </button>
     </pageHeader>
 
+    <pageBody>
+      <div slot="body" class="clearfix mb-20">
+        <div class="pull-left">
+          <p>List of all events</p>
+        </div>
+      </div>
+      <table slot="body" class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Date</th>
+            <th scope="col">Price</th>
+            <th scope="col">Creator</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="event in eventsList" :key="event._id">
+            <td>{{event._id}}</td>
+            <td>{{event.title}}</td>
+            <td>{{event.date}}</td>
+            <td>Rs. {{event.price}}</td>
+            <td>{{event.creator.email}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </pageBody>
+
     <modal :modalId="addEventModalId">
       <h4 slot="header">Add Event</h4>
 
@@ -72,16 +100,20 @@
 
 <script>
 import pageHeader from "../components/resuable/pages_component/pageHeader";
+import pageBody from "../components/resuable/pages_component/pageBody";
 import modal from "../components/resuable/modals/modal";
 
 export default {
   name: "Event",
 
-  components: { pageHeader, modal },
+  components: { pageHeader, pageBody, modal },
+
+  created() {
+    this.fetchEvents();
+  },
 
   data() {
     return {
-
       addEventModalId: "addEventModal",
 
       Breadcrumbs: {
@@ -98,6 +130,8 @@ export default {
           },
         ],
       },
+
+      eventsList: [],
 
       newEvent: {
         title: "",
@@ -119,7 +153,7 @@ export default {
               price: ${+this.newEvent.price}, 
               date: "${this.newEvent.date}"}
             ) {
-              _id, title, description, price, date
+              _id, title, description, price, date, creator{_id, email}
             }
           }
         `,
@@ -132,15 +166,44 @@ export default {
         headers: { "Content-Type": "application/json" },
       })
         .then((res) => {
-          console.log(res);
+          this.eventsList.push(res.data.data.createEvent)
           this.$Toast.fire({
             icon: "success",
-            title: 'Event added.',
+            title: "Event added.",
           });
-          this.newEvent = {title: "", description: "", price: "", date: ""}
+          this.newEvent = { title: "", description: "", price: "", date: "" };
         })
         .catch((err) => {
-          const errorMessage = err.response.data.errors[0].message
+          const errorMessage = err.response.data.errors[0].message;
+          this.$Toast.fire({
+            icon: "error",
+            title: errorMessage,
+          });
+        });
+    },
+
+    fetchEvents: function () {
+      const requestBody = {
+        query: `
+          query {
+            events {
+              _id, title, price, description, date, creator {_id, email}
+            }
+          }
+        `,
+      };
+
+      this.$axios({
+        url: "http://localhost:3000/graphql",
+        data: requestBody,
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => {
+          this.eventsList = res.data.data.events;
+        })
+        .catch((err) => {
+          const errorMessage = err.response.data.errors[0].message;
           this.$Toast.fire({
             icon: "error",
             title: errorMessage,
